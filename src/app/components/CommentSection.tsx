9efe8a5358e8b73e51b1201b8616d5e54b746b3a
ncastelano <ncastelano@gmail.com>
2025-07-09 11:27:19 -0400

    teste98

--- src/app/components/CommentSection.tsx
+++ src/app/components/CommentSection.tsx
@@ -2,6 +2,7 @@
 
 import { useState, useEffect, useRef } from "react";
 import Image from "next/image";
+import { Send } from "lucide-react";
 import { Comment } from "types/comment";
 import {
   collection,
