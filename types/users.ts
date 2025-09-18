5d87f5eff269fe94dc31e17403793b0de8ea85cf
ncastelano <ncastelano@gmail.com>
2025-09-17 20:33:13 -0400

    iuser trainin 8

--- types/users.ts
+++ types/users.ts
@@ -2,13 +2,13 @@
 
 export interface User {
   uid: string;
-  username: string;
   namePage: string;
-  name: string;
-  email: string;
-  image: string;
-  latitude: number | null;
-  longitude: number | null;
-  visible: boolean;
-  viewedBy?: string[]; // UIDs de usuários (logados ou anônimos) que visualizaram o perfil
+  name?: string;
+  email?: string;
+  image?: string;
+  bio?: string;
+  latitude?: number | null;
+  longitude?: number | null;
+  visible?: boolean;
+  createdAt?: Date;
 }
