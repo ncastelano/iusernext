53493b5709410e6fbd9520edccb594001da89565
ncastelano <ncastelano@gmail.com>
2025-09-25 16:26:21 -0400

    iuser training 142

--- src/app/publicar/microfone/page.tsx
+++ src/app/publicar/microfone/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useCallback, useEffect, useRef, useState } from "react";
+import { useCallback, useRef, useState } from "react";
 import { useRouter } from "next/navigation";
 import {
   FaRedo,
